63e8f2a80f1d9ada3301259e1d2144283c84a1e4
Kota Ishimoto <ishikota086@gmail.com>
2016-03-17 17:19:12 +0900

    Add User validation

--- youta/app/models/user.rb
+++ youta/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :student_id, length: { is: 8 }
+  validates :student_id, format: { with: /\A[AB][0-9]{7}/ }
 end
--- youta/spec/models/user_spec.rb
+++ youta/spec/models/user_spec.rb
@@ -1,5 +1,51 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validation" do
+    let(:user) { User.new(params) }
+    let(:params) { {name: name, student_id: sid, admin: admin } }
+
+    describe "when name" do
+      let(:sid) { "A1178086" }
+      let(:admin) { true }
+      context 'is present' do
+        let(:name) { "Kota Ishimoto" }
+        it { expect(user).to be_valid }
+      end
+      context 'is empty' do
+        let(:name) { "" }
+        it { expect(user).not_to be_valid }
+      end
+    end
+
+    describe "when student_id" do
+      let(:name) { "Kota Ishimoto" }
+      let(:admin) { true }
+      context 'is valid' do
+        let(:sid) { "A1178086" }
+        it { expect(user).to be_valid }
+      end
+      context 'does not start with A or B' do
+        let(:sid) { "C1178086" }
+        it { expect(user).not_to be_valid }
+      end
+      context 'is not 8 character' do
+        let(:sid) { "A117808" }
+        it { expect(user).not_to be_valid }
+      end
+      context 'has two alphabet' do
+        let(:sid) { "AB178086" }
+        it { expect(user).not_to be_valid }
+      end
+    end
+  end
+
+  describe "default value" do
+    let(:user) { User.create(name: "Kota Ishimoto", student_id: "A1178086") }
+    specify "admin is false when not supplied" do
+      expect(user.admin).to be_false
+    end
+  end
+
 end
