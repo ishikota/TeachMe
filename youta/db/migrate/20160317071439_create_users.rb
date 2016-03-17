55ab56ca7659c02b74c692ed0ba0a7a0d3cacc65
Kota Ishimoto <ishikota086@gmail.com>
2016-03-17 16:23:44 +0900

    Add User model

---
+++ youta/db/migrate/20160317071439_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :student_id, null:false
+      t.boolean :admin, null: false, default: false
+
+      t.timestamps null: false
+    end
+  end
+end
