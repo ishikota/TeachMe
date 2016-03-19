8d670d35916c6812f04232919acb00646d6dcec4
Kota Ishimoto <ishikota086@gmail.com>
2016-03-20 00:48:32 +0900

    Change visibility of not api method to private

--- youta/app/helpers/import_helper.rb
+++ youta/app/helpers/import_helper.rb
@@ -2,24 +2,25 @@ module ImportHelper
   require 'csv'
 
 
-  # convert csv student_id to array
+  # convert csv forat student ids to User object array
   def read_csv_student_id(filename, initial_password)
     student_ids = read_student_from_csv(filename)
     import_students(student_ids, initial_password)
   end
 
-  def import_students(student_ids, initial_password)
-    def_name = "Sophian"
-    students = []
-    student_ids.map { |student_id| 
-      User.create(student_id: student_id, name: def_name,
-                  password: initial_password, password_digest: initial_password)
-    }.select { |student| !student.blank? }
-  end
+  private
+    def import_students(student_ids, initial_password)
+      def_name = "Sophian"
+      students = []
+      student_ids.map { |student_id| 
+        User.create(student_id: student_id, name: def_name,
+                    password: initial_password, password_digest: initial_password)
+      }.select { |student| !student.blank? }
+    end
 
-  def read_student_from_csv(file_path)
-    data = CSV.read(file_path)
-    data[0]
-  end
+    def read_student_from_csv(file_path)
+      data = CSV.read(file_path)
+      data[0]
+    end
 
 end
--- youta/spec/helpers/import_helper_spec.rb
+++ youta/spec/helpers/import_helper_spec.rb
@@ -17,13 +17,13 @@ describe ImportHelper do
       let(:file_name) { "lecture_students.csv" }
       let(:file_path) { fixture_file_upload(file_name, 'text/csv') }
       let(:result) { %w"A1178086 A1178087 A1178088" }
-      it { expect(helper.read_student_from_csv(file_path)).to eq result }
+      it { expect(helper.send(:read_student_from_csv, file_path)).to eq result }
     end
 
     let(:student_ids) { %w"A1178086 A1178087 A1178088" }
     let(:password) { "foobar" }
     it "should convert student_ids to User object array" do
-      students = helper.import_students(student_ids, password)
+      students = helper.send(:import_students, student_ids, password)
       expect(User.count).to eq student_ids.size
       students.each_with_index { |student, idx|
         expect(student.student_id).to eq student_ids[idx].downcase
