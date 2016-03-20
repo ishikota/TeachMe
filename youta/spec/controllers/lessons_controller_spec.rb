fbc77cb267e66123d555d104bd2df1cf2c4bdcea
Kota Ishimoto <ishikota086@gmail.com>
2016-03-20 17:46:21 +0900

    Implement index action

--- youta/spec/controllers/lessons_controller_spec.rb
+++ youta/spec/controllers/lessons_controller_spec.rb
@@ -3,9 +3,13 @@ require 'spec_helper'
 describe LessonsController do
 
   describe "GET 'index'" do
-    it "returns http success" do
+    before {
+      Lesson.create(title: "sansu", day_of_week: 0, period: 1)
+      Lesson.create(title: "kokugo", day_of_week: 1, period: 2)
+    }
+    it "should assigns all lessons to @lessons" do
       get 'index'
-      response.should be_success
+      expect(assigns(:lessons)).to eq Lesson.all
     end
   end
 
