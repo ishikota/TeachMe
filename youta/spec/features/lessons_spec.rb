7b606669371650ac0c0ee105c6575559342c0ff4
Kota Ishimoto <ishikota086@gmail.com>
2016-03-20 19:06:52 +0900

    Add feature spec for lesson pages

---
+++ youta/spec/features/lessons_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature "Lessons", :type => :feature do
+
+  it 'visit page wit no-lesson item' do
+    visit lessons_index_path
+    expect(page).to have_content '授業一覧'
+  end
+end
