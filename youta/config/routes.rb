a7d6cc05e6d1c558265f9d7b48386d20daef2a3f
Kota Ishimoto <ishikota086@gmail.com>
2016-03-21 19:48:50 +0900

    Generate QuestionsController

--- youta/config/routes.rb
+++ youta/config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
-  resources :lessons
+  resources :lessons do
+    resources :questions
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
