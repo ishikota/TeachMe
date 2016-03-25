0b635cfda07e0927f9ba789dcbd358cadf9c37b0
Kota Ishimoto <ishikota086@gmail.com>
2016-03-25 16:57:07 +0900

    Implement user update feature

--- youta/spec/controllers/users_controller_spec.rb
+++ youta/spec/controllers/users_controller_spec.rb
@@ -13,4 +13,22 @@ RSpec.describe UsersController, :type => :controller do
     end
   end
 
+  describe "#edit" do
+    it "should assign user" do
+      #TODO get user_path(user) raises No route matches {:action=>"/users/1", :controller=>"users"}
+      get :edit, id: user.id
+      expect(assigns(:user)).to eq user
+    end
+  end
+
+  describe "#update" do
+    it "should update user and redirect to my page" do
+      params = { user: { student_id: "a1178086", name: "Kota", password: 'foobar', password_confirmation: 'foobar' } }
+      pending 'No route matches {:action=>"/users/1", :controller=>"users",...'
+      put user_path(user), params
+      expect(response).to redirect_to :show
+    end
+  end
+
+
 end
--- youta/spec/features/users_spec.rb
+++ youta/spec/features/users_spec.rb
@@ -10,12 +10,36 @@ feature "Users", :type => :feature do
     fill_in '学生番号', with: user.student_id
     fill_in 'パスワード', with: user.password
     click_button 'ログイン'
-    visit user_path(user)
   }
 
-  it 'should display user information' do
-    expect(page).to have_content user.name
-    expect(page).to have_button '編集'
+  describe "#show" do
+    it 'should display user information' do
+      visit user_path(user)
+      expect(page).to have_content user.name
+      expect(page).to have_link '編集'
+    end
+  end
+
+  describe "#edit & #update" do
+    let(:new_name) { 'Ishimoto Kota' }
+    let(:password) { 'foobar' }
+    it "should update user name" do
+      visit edit_user_path(user)
+      fill_in '名前', with: new_name
+      fill_in 'パスワード', with: password
+      fill_in 'パスワードの確認', with: password
+      click_on '更新'
+      expect(page).not_to have_content '登録情報の編集'
+      expect(page).to have_content new_name
+    end
+    it "should fail to update" do
+      visit edit_user_path(user)
+      fill_in '名前', with: new_name
+      fill_in 'パスワード', with: password
+      fill_in 'パスワードの確認', with: 'barfoo'
+      click_on '更新'
+      expect(page).to have_content '登録情報の編集'
+    end
   end
 
 end
