46a84bf619a471fc923f8857106a60693b2a9bf5
Kota Ishimoto <ishikota086@gmail.com>
2016-03-25 12:29:36 +0900

    Create CommentsController with create action

--- youta/config/routes.rb
+++ youta/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :lessons do
     resources :questions
   end
+  resources :comments, only: :create
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
