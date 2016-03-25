7896f9b89683494d0121ba118410c3866fd80123
Kota Ishimoto <ishikota086@gmail.com>
2016-03-25 15:35:46 +0900

    Add users#show action

--- youta/spec/controllers/users_controller_spec.rb
+++ youta/spec/controllers/users_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
+  let!(:user) {
+    User.create(name: "Kota Ishimoto", student_id: "A1178086", admin: true, password: 'foobar', password_confirmation: 'foobar')
+  }
+
+  describe "#show" do
+    it "should assign user" do
+      #TODO get user_path(user) raises No route matches {:action=>"/users/1", :controller=>"users"}
+      get :show, id: user.id
+      expect(assigns(:user)).to eq user
+    end
+  end
 
 end
